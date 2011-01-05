a88abe444057b5f9e33b07d38a74a417adcdee60
MgaMPKAy <mgampkay@163.com>
2011-01-05 16:05:26 +0800

    new file:   thq/final/12-2.c  	modified:   thq/final/14_simple_linked_list.c

--- thq/final/14_simple_linked_list.c
+++ thq/final/14_simple_linked_list.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <signal.h>
 
 typedef struct student *link;
 struct student{
@@ -23,12 +22,12 @@ link find_student(long id);
 void add_student(long, int, int, int);
 void lsort(void);
 
-void get_input(const char * type, void *,int func(void *, char*), char * greet);
-int greater_than_0(void *, char *);
-int long_greater_than_0(void *, char*);
-int int_greater_than_0(void *, char*);
-int range0_4(void *,char*);
-int yesno(void *, char *);
+void get_input(char *, void *,int func(char*, void*), char *);
+/* helper func for get_input */
+int greater_than_0(char*, void*);
+int range0_4(char*, void*);
+int yesno(char*, void*);
+int ok(char*, void*);
 
 link lstud;
 
@@ -60,7 +59,7 @@ void menu_main(void)
 		/* */
 		
 		choice = -1;
-		get_input("int", &choice, range0_4,
+		get_input("%d", &choice, range0_4,
 			  "Please select (0 ~ 4): ");
 		
 		switch (choice){
@@ -83,15 +82,15 @@ void menu_add(void)
 	while (!0){
 		printf("------------------------------------------------\n");
 		
-		get_input("long", &id, greater_than_0,
+		get_input("%ld", &id, greater_than_0,
 			  "Enter student's id: ");
 		/*get_input("char*", name, NULL,
 		  "Enter student's name: "); */
-		get_input("int", &score[0], greater_than_0,
+		get_input("%d", &score[0], greater_than_0,
 			  "Enter English score: ");
-		get_input("int", &score[1], greater_than_0,
+		get_input("%d", &score[1], greater_than_0,
 			  "Enter Math score: ");
-		get_input("int", &score[2], greater_than_0,
+		get_input("%d", &score[2], greater_than_0,
 			  "Enter Computer score: ");
 		
 		if (!find_student(id)){
@@ -103,7 +102,7 @@ void menu_add(void)
 		}
 		
 		
-		get_input("char", &choice, yesno,
+		get_input("%c", &choice, yesno,
 			  "Add another student? (y / n)");
 		if (choice == 'n' || choice == 'N')
 			return;
@@ -117,7 +116,7 @@ void menu_find(void)
 	struct student *stud;
 	while(!0){
 		printf("------------------------------------------------\n");
-		get_input("long", &id, greater_than_0,
+		get_input("%ld", &id, greater_than_0,
 			  "Enter student's id: ");
 		if ((stud = find_student(id))){
 			printf("  Student     English   Math    Comp.   Total\n");
@@ -127,7 +126,7 @@ void menu_find(void)
 		else{
 			printf("!!!Error: can't find this student\n");
 		}
-		get_input("char", &choice, yesno,
+		get_input("%c", &choice, yesno,
 			  "Find another student? (y / n)");
 		if (choice == 'n' || choice == 'N')
 			return;
@@ -137,8 +136,9 @@ void menu_find(void)
 void menu_list(void)
 {
 	link iter = lstud;
+	char tmp[2];
        	if (iter->next == NULL){
-		get_input("none", NULL, NULL, "No student");
+		get_input("%c", tmp, ok, "No student");
 		return;
 	}
 	printf("------------------------------------------------\n");
@@ -149,7 +149,7 @@ void menu_list(void)
 		       iter->math,  iter->comp, iter->total);
 		iter = iter->next;
 	}
-	get_input("none", NULL, NULL, "Press any key to continue ");
+	get_input("%c", tmp, ok, "Press any key to continue ");
 }
 void add_student(long id, int engl, int math, int comp)
 {
@@ -197,66 +197,32 @@ void lsort(void)
 	}
 }
 
-
-void get_input(const char * type, void * data, int func(), char * greet)
+void get_input(char *pattern, void *containr, int ok(char*, void*), char *greet)
 {
-	// change scanf to fgets + sscanf
-	int ire = -1;
-	long lre = -1;
-	char *input = malloc(sizeof(char) * 80), ch;
-	int tmp;
-	if (strcmp(type, "int") == 0){
-		do {
-			printf("%s", greet);
-			scanf("%d", &ire);
-			while((tmp = getchar()) != (int)'\n' && tmp != EOF);
-		} while (!func(&ire, type));
-		*(int *)data = ire;
+	char input[80];
+	if (strcmp(pattern, "none") == 0){
+		fgets(input, 79, stdin);
 		return;
 	}
-	if (strcmp(type, "long") == 0){
-		do {
-			printf("%s", greet);
-			scanf("%ld", &lre);
-			while((tmp = getchar()) != (int)'\n' && tmp != EOF);
-		} while (!func(&lre, type));
-		*(long*)data = lre;
-		return;
-	}
-	if (strcmp(type, "char") == 0){
-		do {
-			printf("%s", greet);
-			scanf("%c", &ch);
-			while((tmp = getchar()) != (int)'\n' && tmp != EOF);
-		} while (!func(&ch, type));
-		*(char*)data = ch;
-		return;
-	}
-	if (strcmp(type, "none") == 0){
+	do {
+	begin:
 		printf("%s", greet);
-		while((tmp = getchar()) != (int)'\n' && tmp != EOF);
-	}
-	/* bug 
-	if (strcmp(type, "char*") == 0){
-		printf("%s", greet);
-		fgets(input, 25, stdin);
-		*(char *)data = input; //bug
-		return;
-	}
-	*/
-	free(input);
+		fgets(input, 79, stdin);
+		if (sscanf(input, pattern, containr) != 1)
+			goto begin;
+	}while (!ok(pattern, containr));	
 }
 
-int greater_than_0(void * data, char * type)
+int greater_than_0(char *pattern, void *data)
 {
-	if (strcmp(type, "long") == 0){
-		if (*(long *)data > 0)
+	if (strcmp(pattern, "%ld") == 0){
+		if (*(long *)data >= 0)
 			return 1;
 		else
 			return 0;
 	}
-	if (strcmp(type, "int") == 0){
-		if (*(int *)data > 0)
+	if (strcmp(pattern, "%d") == 0){
+		if (*(int *)data >= 0)
 			return 1;
 		else
 			return 0;
@@ -264,9 +230,9 @@ int greater_than_0(void * data, char * type)
 	return 0;
 }
 
-int range0_4(void * data, char *type)
+int range0_4(char *pattern, void *data)
 {
-	if (strcmp(type, "int") == 0){
+	if (strcmp(pattern, "%d") == 0){
 		if (*(int *)data >= 0 && *(int *)data <= 4)
 			return 1;
 		else
@@ -275,10 +241,10 @@ int range0_4(void * data, char *type)
 	return 0;
 }
 
-int yesno(void *data, char *type)
+int yesno(char *pattern, void *data)
 {
 	char c = *(char *)data;
-	if (strcmp(type, "char") == 0){
+	if (strcmp(pattern, "%c") == 0){
 		if (c == 'y' || c =='Y'
 		    || c == 'n' || c == 'N')
 			return 1;
@@ -288,9 +254,9 @@ int yesno(void *data, char *type)
 	return 0;
 }
 
-
-
-
-
+int ok(char* pattern, void* data)
+{
+	return 1;
+}
 
 
