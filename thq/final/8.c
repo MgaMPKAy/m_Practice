974c6b307ede51ae2b98e812daee7465771b34c8
MgaMPKAy <mgampkay@163.com>
2011-01-09 19:26:51 +0800

    modified:   8.c

--- thq/final/8.c
+++ thq/final/8.c
@@ -6,9 +6,11 @@ int main(void)
 	for (r = 1, s = 0; r <= 3; r++){
 		for (w = 0; w <= 3; w++){
 			for (b = 0; b <= 6; b++){
-				printf("Red : %d, White : %d, Black : %d\n",
-				       r, w, b);
-				s++;
+				if(r + w + b == 6){
+					printf("Red : %d, White : %d, Black : %d\n",
+					       r, w, b);
+					s++;
+				}
 			}
 		}
 	}
