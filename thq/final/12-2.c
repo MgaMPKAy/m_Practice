4f442e6c8aae8113b9b74507437e4edfc6b86308
adlezz <adlezzme@gmail.com>
2011-01-10 21:49:15 +0800

    modified:   12-2.c 简化

--- thq/final/12-2.c
+++ thq/final/12-2.c
@@ -1,29 +1,19 @@
 /* 判断任意整数是否是回文*/
 #include <stdio.h>
 #include <string.h>
-#define N 1000
-
-/* 判断回文 */
-int is_palindrome(char *str)
-{
-	int i, j;
-	for(i = 0, j = strlen(str) - 1; i < j; i++, j--){
-		if (str[i] != str[j])
-			return 0;
-	}
-	return 1;
-}
-
 int main(void)
 {
 	char str[80];
-
+	int i, j, flag = 0;
 	printf("Enter a num: ");
 	scanf("%s", str);
-	if (is_palindrome(str))
-		printf("%s is a palindrome\n", str);
-	else
-		printf("%s isn't a palindrome\n", str);
-
+	
+	for(i = 0, j = strlen(str) - 1; i < j; i++, j--){
+		if (str[i] != str[j]){
+			printf("%s isn't a palindrome\n", str);
+			return 0;
+		}
+	}
+	printf("%s is a palindrome\n", str);
 	return 0;
 }
