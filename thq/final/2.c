b1082a3db89a3af699e18ae498b41838c6ac0dcd
adlezz <adlezzme@gmail.com>
2011-01-10 22:01:46 +0800

    modified:   2.c

--- thq/final/2.c
+++ thq/final/2.c
@@ -2,28 +2,14 @@
 
 int main(void)
 {
-	float n = 1, c;
-	int i;
-
-	for (i = 0, c = 6, n = 1; n < 2; i++){
-		n *= (1 + c / 100);
-	}
-	printf("When c = %2.0f%%, needs %d years\n", c, i);
-
-	for (i = 0, c = 8, n = 1; n < 2; i++){
-		n *= (1 + c / 100);
-	}
-	printf("When c = %2.0f%%, needs %d years\n", c, i);
-
-	for (i = 0, c = 10, n = 1; n < 2; i++){
-		n *= (1 + c / 100);
-	}
-	printf("When c = %2.0f%%, needs %d years\n", c, i);
-
-	for (i = 0, c = 12, n = 1; n < 2; i++){
-		n *= (1 + c / 100);
-	}
-	printf("When c = %2.0f%%, needs %d years\n", c, i);
+	float n = 1, c[4] = {6, 8, 10, 12};
+	int i, j;
 	
+	for (i = 0; i < 4; i++){
+		for (j = 0, n = 1; n < 2; j++){
+			n *= (1 + c[i]/100);
+		}
+		printf("When c = %2.0f%%, needs %d years\n", c[i], j);
+	}
 	return 0;
 }
