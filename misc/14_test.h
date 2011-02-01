ea5494b378750292ec66621f4a79cb84c95681c1
MgaMPKAy <mgampkay@163.com>
2011-02-01 16:27:29 +0800

    deleted:    14_test.c 14_test.h

--- misc/14_test.h
+++
@@ -1,7 +0,0 @@
-#define list_entry(ptr, type, member)					\
-	((type *)((char *)(ptr)-(unsigned long)(&((type *)0)->member)))
-
-#define list_for_each(pos, head)				\
-	for (pos = (head)->next; pos != NULL; pos = pos->next)
-
-void test(void);
