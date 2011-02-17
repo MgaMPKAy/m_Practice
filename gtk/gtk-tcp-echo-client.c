1e403689a0ecc44b46ed7f54436114617d2c4b9f
MgaMPKAy <mgampkay@163.com>
2011-02-17 10:05:46 +0800

    a gtk+ echo client and a command-line echo server remove 14_pthread.c

---
+++ gtk/gtk-tcp-echo-client.c
@@ -0,0 +1,97 @@
+#include <gtk/gtk.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int sock;
+struct sockaddr_in server;
+struct hostent *hp;
+unsigned short port;
+gchar buffer[1024];
+GtkWidget *entry;
+
+void error(char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+void socket_send_msg(GtkWidget *widget, gpointer *entry)
+{
+	int n;
+	const gchar *msg = gtk_entry_get_text(GTK_ENTRY(entry));
+	n = send(sock, msg, strlen(msg),0);
+	if (n < strlen(msg))
+		error("Writing to socket");
+}
+
+void socket_recv_msg(GtkWidget *widget, gpointer *entry)
+{
+	int n;
+	n = recv(sock, buffer, 1023, 0);
+	gchar *msg;
+	if (n < 1) error("reading from socket");
+	buffer[n]='\0';
+	msg = buffer;
+	gtk_entry_set_text(GTK_ENTRY(entry), msg);
+}
+
+int main(int argc, char *argv[])
+{
+	GtkWidget *window;
+	GtkWidget *vbox;
+	GtkWidget *button;
+	
+	gtk_init(&argc, &argv);
+
+	if (argc != 3) { 
+		printf("Usage: %s server port\n", argv[0]);
+		exit(1);
+	}
+	sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock < 0) error("Opening socket");
+	
+	server.sin_family = AF_INET;
+	hp = gethostbyname(argv[1]);
+	if (hp == NULL)
+		error("Unknown host");
+	
+	memcpy((char *)&server.sin_addr,(char *)hp->h_addr_list,         
+		  hp->h_length);
+	port = (unsigned short)atoi(argv[2]);
+	server.sin_port = htons(port);
+	if (connect(sock, (struct sockaddr *)&server, sizeof server) < 0)
+		error("Connecting");
+	
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	g_signal_connect(G_OBJECT(window), "destroy",
+			 G_CALLBACK(gtk_main_quit), NULL);
+
+	vbox = gtk_vbox_new(TRUE, 0);
+	gtk_container_add(GTK_CONTAINER(window), vbox);
+	
+	entry = gtk_entry_new();
+	gtk_box_pack_start(GTK_BOX(vbox), entry, TRUE, TRUE, 0);
+	
+	button = gtk_button_new_with_label("Send");
+	gtk_box_pack_start(GTK_BOX(vbox), button, TRUE, TRUE, 0);
+	g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(socket_send_msg),
+			 entry);
+	g_signal_connect(G_OBJECT(button), "clicked", G_CALLBACK(socket_recv_msg),
+			 entry);
+	
+	gtk_widget_show_all(window);
+	gtk_main();
+
+	if (close(sock) < 0) error("closing");
+	printf("Client terminating\n");
+	
+	return 0;
+}
+
