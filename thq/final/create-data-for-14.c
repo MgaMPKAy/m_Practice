e2a173065dcfc4a9a6d39d8df7f1695c1ee4934a
MgaMPKAy <mgampkay@163.com>
2011-02-20 15:15:48 +0800

    new file:   misc/14-gtk.c 	new file:   thq/final/create-data-for-14.c

---
+++ thq/final/create-data-for-14.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+
+int main(int argc, char *argv[])
+{
+	long n;
+	int name_len;
+	FILE *fd;
+	char output[80];
+	char name[20];
+	
+	if (argc >= 2) {
+		n = atoi(argv[1]);
+		printf("%ld\n", n);
+	} else {
+		printf("Error\n");
+		exit(1);
+	}
+	fd = fopen("./data", "w");
+	srand((unsigned long)time(NULL));
+	for (; n > 0; n--) {
+		name_len = rand() % 19 + 1;
+		name[name_len--] = '\0';
+		for (; name_len >= 0; name_len--) {
+			name[name_len] = 65 + rand() % 26;
+		}
+		sprintf(output, "%ld %d %d %d %s\n",
+			n, rand() % 101, rand() % 101, rand() % 101,
+			name);
+		fputs(output, fd);
+	}
+	fclose(fd);
+	return 0;
+}
