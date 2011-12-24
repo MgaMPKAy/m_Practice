57bfc52c370599e6f0a46390a9b582cb1936c839
MgaMPKAy <mgampkay@163.com>
2011-12-24 23:36:09 +0800

    xmonad backup

--- hasekll/xmonad.hs
+++ hasekll/xmonad.hs
@@ -1,4 +1,3 @@
-
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
@@ -8,46 +7,48 @@ import System.IO
 
 myManageHook :: ManageHook
 myManageHook = composeAll
-    [ className =? "Gimp"      --> doFloat
---  , className =? "Vncviewer" --> doFloat
-    , className =? "Gvbam"     --> doFloat
-    , className =? "Eog"       --> doFloat
-    , className =? "Java"      --> doFloat
+    [ className =? "Gvbam"     --> doFloat
+    , className =? "FloatWindow"      --> doFloat
     ]
 
 myKeys :: [(String, X ())]
-myKeys = [ ("C-<Print>",              spawn "sleep 0.2; gnome-screenshot -a -i")
-         , ("<Print>",                spawn "a-screenshot")
-         , ("M1-0",                   spawn "urxvtc -cd /tmp/workspace")
+myKeys = [ ("M-w",                    kill)
+         , ("M1-0",                   spawn "urxvtc")
          , ("M1--",                   spawn "a-ex")
          , ("M1-=",                   spawn "chromium --user-data-dir=/tmp/chromium --incognito")
          , ("M1-C-=",                 spawn "chromium --user-data-dir=/tmp/chromium")
-         -- , ("M-p",	 	      spawn "dmenu_run | dmenu -b")
          , ("M1-<F9>",                spawn "a-touchpad")
+         , ("<Print>",                spawn "a-screenshot")
          , ("<XF86Tools>",            spawn "a-touchpad")
          , ("<XF86AudioMute>",        spawn "a-mixer-mute")
          , ("<XF86AudioLowerVolume>", spawn "a-mixer-down")
          , ("<XF86AudioRaiseVolume>", spawn "a-mixer-up")
-         , ("M-w",                    kill)
+         --, ("M-p",                 spawn "dmenu_run | dmenu -b")
          ]
+
+myXmobar :: Handle -> PP
+myXmobar proc = xmobarPP {
+                  ppOutput = hPutStrLn proc
+                , ppTitle = xmobarColor "green" "" . shorten 50
+                }
+
+myStartupHook :: X ()
+myStartupHook = spawn "a-touchpad"
 
 main :: IO ()
 main = do
     xmproc <- spawnPipe "xmobar"
     xmonad $ defaultConfig
         { manageHook = manageDocks <+> myManageHook
-                        <+> manageHook defaultConfig
+                       <+> manageHook defaultConfig
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
-        , logHook = dynamicLogWithPP $ xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 50
-                        }
+        , logHook = dynamicLogWithPP $ myXmobar xmproc
         , modMask = mod4Mask
         , normalBorderColor = "#dddddd"
         , focusedBorderColor = "#cdad00"
         , focusFollowsMouse = False
         , terminal = "urxvt"
-        , startupHook = do {spawn "a-touchpad"}
+        , startupHook = myStartupHook
         } `additionalKeysP` myKeys
 
 {-
