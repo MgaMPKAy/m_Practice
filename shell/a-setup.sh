b6d01b4ad7c560fdc71d065d2b16a5a3738a1228
MgaMPKAy <mgampkay@163.com>
2012-02-06 09:54:35 +0800

    a-setup: check if collections are setuped

--- shell/a-setup.sh
+++ shell/a-setup.sh
@@ -1,51 +1,72 @@
 #!/bin/bash
 
-OLDPWD=$(pwd)
-TMPROOT=/tmp/tmproot
-ARCDIR=~/misc/bin
-PROGRAM=$1
+U_ARCHIVES_DIR=$KFAT1/bin
+MISC_ARCHIVES_DIR=~/misc/bin
+TMP_ROOT=/tmp/tmproot
+TMP_BIN_PATH=$TMP_ROOT/bin
+SETUPED_FILE=$TMP_ROOT/setuped
 
-function setup() {
-    BINNAME=$1
-    if [ -n "$2" ] ; then
-	FILENAME=$2
-    fi
-	
-    AREADY=$(ls $TMPROOT/bin/ | grep $BINNAME)
-    if [ -n "$AREADY" ] ; then
-	echo "Alread Setup"
-	return
-    else
-	if [ -z "$FILENAME" ]; then
-	    FILENAME=$(ls ~/misc/bin/ | grep -i $BINNAME | head -n 1)
+## Handle args
+if [ -z "$1" ]; then
+    echo "Usage: a-setup target"
+    exit
+fi
+
+## Special setups
+case "$1" in
+    "list")
+	if [ -e $U_ARCHIVES_DIR ]; then
+	    echo "U:"
+	    ls $U_ARCHIVES_DIR
 	fi
-	tar xf $ARCDIR/$FILENAME
-    fi
-}
-
-cd /tmp/tmproot/
-
-case "$PROGRAM" in
-    "mplayer" ) 
-	setup "mplayer" 
+	echo "MISC:"
+	ls $MISC_ARCHIVES_DIR
+	exit
 	;;
-    "redis" )
-	setup "redis"
+    "kfat1")
+	a-mount-kfat1
+	exit
 	;;
-    "nginx" )
-	setup "nginx"
+    "devfs")
+	a-mount-devfs
+	exit
 	;;
-    "ocaml" ) 
-	setup "ocaml"
-	;;
-    "php" )
-	setup "php"
-	;;
-    "racket" )
-	setup "racket"
-	;;
-    
 esac
 
-cd $OLDPWD
+TARGET=$1
 
+## Check if setuped
+if [ -e $SETUPED_FILE ]; then
+    SETUPED=$(grep -i $TARGET $SETUPED_FILE)
+else
+    SETUPED=""
+fi
+if [ -n "$SETUPED" ]; then
+    echo "$SETUPED already setuped"
+    exit
+fi
+
+## Set up source
+# Try U_ARCHIVES_DIR
+if [ -e $U_ARCHIVES_DIR ]; then
+    ARCHIVE_FILE=$(ls $U_ARCHIVES_DIR | grep -i $TARGET)
+    ARCHIVES_DIR=$U_ARCHIVES_DIR
+fi
+
+# Try ARCHIVES_DIR
+if [ -z "$ARCHIVE_FILE" ]; then
+    ARCHIVE_FILE=$(ls $MISC_ARCHIVES_DIR | grep -i $TARGET)
+    ARCHIVES_DIR=$MISC_ARCHIVES_DIR
+fi
+
+if [ -z "$ARCHIVE_FILE" ]; then
+    echo "Can not found $TARGET"
+    exit
+fi
+
+## Set up target
+CURRENT_DIR=$(pwd)
+cd $TMP_ROOT
+echo copy from $ARCHIVES_DIR/$ARCHIVE_FILE
+tar xf $ARCHIVES_DIR/$ARCHIVE_FILE && echo $ARCHIVE_FILE >> $SETUPED_FILE
+cd $CURRENT_DIR
